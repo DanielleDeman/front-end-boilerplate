7235f13a36f38e87a0ca007e0a86ae760e5da380
Danielle Maas-de Man <danielle.deman@outlook.com>
2024-12-29 10:03:53 +0100

    test: responsive query

--- app/components/Example/ApiResponse/RickAndMorty.vue
+++ app/components/Example/ApiResponse/RickAndMorty.vue
@@ -2,11 +2,13 @@
 import type { UseApiDataOptions } from '#build/module/nuxt-api-party'
 import type { Character, Info } from 'rickmortyapi'
 
+const page = ref(3)
+
 const { data } = await useRickAndMortyData<Info<Character[]>>('character', {
-  key: () => 'character-page-2',
-  query: {
-    page: 2,
-  },
+  key: () => 'character-page-3',
+  query: computed(() => ({
+    page: page.value,
+  })),
 } as UseApiDataOptions<Info<Character[]>>)
 </script>
 
