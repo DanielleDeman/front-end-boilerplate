b583e73e52be90e89ed068dd2f66d35243f735ef
Danielle Maas-de Man <danielle.deman@outlook.com>
2024-12-29 13:22:51 +0100

    fix: remove unused code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,14 +9,6 @@
       <div class="mb-12">
         <p>Have a good time reviewing my code!</p>
       </div>
-      <div class="grid gap-4 xl:grid-cols-2">
-        <section title="Rick and Morty API response">
-          <ExampleApiResponseRickAndMorty />
-        </section>
-        <section title="Pokemon API response">
-          <ExampleApiResponsePokemon />
-        </section>
-      </div>
     </UContainer>
   </div>
 </template>
