60e4d002622ba9597a6ac84b253bb511be67840f
Danielle Maas-de Man <danielle.deman@outlook.com>
2024-12-29 12:07:02 +0100

    fix: pagination

--- app/components/Example/ApiResponse/RickAndMorty.vue
+++ app/components/Example/ApiResponse/RickAndMorty.vue
@@ -12,16 +12,25 @@ const { data, error } = await useRickAndMortyData<Info<Character[]>>('character'
   query,
 } as UseApiDataOptions<Info<Character[]>>)
 
+const { data: otherData, error: otherError } = await useRickAndMortyData<Info<Character[]>>(`character?page=${page.value}`, {
+  key: () => `character-page-other-${page.value}`,
+} as UseApiDataOptions<Info<Character[]>>)
+
 watch(data, () => {
   console.log('rm data', page.value, query.value, data?.value?.results?.length, data?.value?.results?.[0]?.name, error?.value)
+}, { immediate: true })
+
+watch(otherData, () => {
+  console.log('rm otherdata', page.value, otherData?.value?.results?.length, otherData?.value?.results?.[0]?.name, otherError?.value)
 }, { immediate: true })
 </script>
 
 <template>
   <div>
     <ExampleApiResponse :response="data" />
+    <ExampleApiResponse :response="otherData" />
     <UButton
-      color="green"
+      color="red"
       size="xl"
       @click="page = page + 1"
     >
